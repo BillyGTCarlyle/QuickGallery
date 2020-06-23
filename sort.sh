50730b5892ddd7eec0158aba94cae54321a8ef88
Billy Carlyle <billy@billycarlyle.uk>
2020-06-23 12:14:16 +0100

    Added prompt for captions.

--- sort.sh
+++ sort.sh
@@ -5,13 +5,14 @@ do
 if [ ! -f images/thumbnails/${i#"images/"} ]; then
 	convert $i -geometry x200 images/thumbnails/${i#"images/"}
 	convert $i -geometry x1080 images/smaller/${i#"images/"}
+	read -p "Caption for ${i#"images"}:" caption
 
 	sed -i "/<h1>Images<\/h1>/a\
 \<div class=\"responsive\"\>\n\
 	\<a href=\"$i\"\>\n\
 	\<div class=\"gallery\"\>\n\
 		\<img src=\"images\/thumbnails\/${i#"images/"}\" alt=\"\"\>\n\
-		\<div class=\"desc\"\>\<\/div\>\n\
+		\<div class=\"desc\"\>$caption\<\/div\>\n\
 	\<\/div\>\n\
 	\<\/a\>\n\
 \<\/div\>" images.html

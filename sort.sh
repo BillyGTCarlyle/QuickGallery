eff6fb8354ac2c76eb5519c98111a2ce19fa7df7
Billy Carlyle <billy@billycarlyle.uk>
2020-07-09 18:30:53 +0100

    Scipt now looks for a comment.

--- sort.sh
+++ sort.sh
@@ -7,7 +7,7 @@ if [ ! -f images/thumbnails/${i#"images/"} ]; then
 	convert $i -geometry x1080 images/smaller/${i#"images/"}
 	read -p "Caption for ${i#"images"}:" caption
 
-	sed -i "/<h1>Images<\/h1>/a\
+	sed -i "/<!-- gal -->/a\
 \<div class=\"responsive\"\>\n\
 	\<a href=\"$i\"\>\n\
 	\<div class=\"gallery\"\>\n\
